8c1be80e5b9743d403565ac5eafc600076e825d4
annalaravind <annaldasaravind64@gmail.com>
2024-09-02 19:49:55 +0530

    routing

---
+++ app/components/Footer.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "../globals.css";
+
+const Footer = () => {
+  return <div>Footer</div>;
+};
+
+export default Footer;
---
+++ app/components/Navbar.jsx
@@ -0,0 +1,44 @@
+"use client";
+
+import React from "react";
+import "../globals.css";
+import Image from "next/image";
+import NavLogo from "../../public/amazon.svg";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+const Navbar = () => {
+  const pathname = usePathname();
+  console.log(pathname);
+  return (
+    <div className="next-section header">
+      <div className="next-container">
+        <div className="next-wrapper flex-row">
+          <div className="navbar-logo-ctn">
+            <Image
+              className="navbar-logo"
+              src={NavLogo}
+              alt="amazon-logo"
+              loading="eager"
+            />
+          </div>
+          <div className="navbar-menu-ctn">
+            <ul>
+              <li className={pathname == "/about" ? "current-page" : ""}>
+                <Link href="/about">About</Link>
+              </li>
+              <li className={pathname == "/contact" ? "current-page" : ""}>
+                <Link href="/contact">Contact</Link>
+              </li>
+              <li className={pathname == "/products" ? "current-page" : ""}>
+                <Link href="/products">Products</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
